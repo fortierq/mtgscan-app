589566466da96a4299c501ed283946b4b021e6ea
Quentin Fortier <qpfortier@gmail.com>
2021-01-03 22:17:29 +0100

    Load cards ini __init__

--- mtgscan_app/__init__.py
+++ mtgscan_app/__init__.py
@@ -1,1 +1,7 @@
 __version__ = '0.1.0'
+
+from mtgscan.ocr import Azure
+from mtgscan.text import MagicRecognition
+
+azure = Azure()
+rec = MagicRecognition()
--- mtgscan_app/scan.py
+++ mtgscan_app/scan.py
@@ -1,10 +1,7 @@
-from mtgscan.ocr import Azure
-from mtgscan.text import MagicRecognition
+from . import azure, rec
 
 
 def scan(image):
-    azure = Azure()
-    rec = MagicRecognition()
     box_texts = azure.image_to_box_texts(image)
     deck = rec.box_texts_to_deck(box_texts)
     return deck
