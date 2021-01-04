8cffe817967ff3eb0486a69bff0238712047c8ea
Quentin Fortier <qpfortier@gmail.com>
2021-01-04 12:58:47 +0100

    Use url_for

--- mtgscan_app/app.py
+++ mtgscan_app/app.py
@@ -1,8 +1,8 @@
 import os
-import sys
 from pathlib import Path
 
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import (Flask, flash, redirect, render_template, request,
+                   send_from_directory, url_for)
 from werkzeug.utils import secure_filename
 
 from mtgscan_app.scan import scan
@@ -11,35 +11,33 @@ UPLOAD_FOLDER = str(Path(__file__).parent / "dl")
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-# default value during development
-# app.secret_key = b'\xff(\x13\x96\xa7U\xb2\x14B\tZ\x0em\xaa\xc7\x08'
-# overridden if this file exists in the instance folder
-# app.config.from_pyfile('config.py', silent=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 100_000_000
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    deck = "no deck"
-    if request.method == 'POST':    
-        # check if the post request has the file part
+    deck, filename = "", ""
+    print(os.listdir("."))
+    if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
         if file and allowed_file(file.filename):
-            print(file)
             filename = secure_filename(file.filename)
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(path)
             deck = scan(path)
-            return render_template("upload.html", deck=deck)
-    return render_template("upload.html", deck=deck)
+    return render_template("upload.html", deck=deck, image=filename)
