a1bbab3250d1b500aaf8391a8edf3323ab8e399e
Quentin Fortier <qpfortier@gmail.com>
2021-01-19 21:21:45 +0100

    Revert wtform

--- mtgscan_app/app.py
+++ mtgscan_app/app.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 from flask import (Flask, flash, redirect, render_template, request,
                    send_from_directory, url_for)
-from flask.ext.wtf import Form
-from wtforms import URL, FileField
 from werkzeug.utils import secure_filename
 
 from mtgscan_app.scan import scan
@@ -20,10 +18,6 @@ app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-class URLForm(Form):
-    name = URL('URL')
-    submit = FileField('Scan')
-
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
@@ -32,7 +26,6 @@ def uploaded_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     deck, filename = "", ""
-    url_form = URLForm()
     if request.method == 'POST':
         path = None
         if 'file' in request.files and request.files['file']:
@@ -40,10 +33,9 @@ def upload_file():
             filename = secure_filename(file.filename)
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(path)
-        elif url_form.validate_on_submit(): #"url_image" in request.form and request.form["url_image"]:
-            path = url_form.name.data
-            url_form.name.data = ''
+        elif "url_image" in request.form and request.form["url_image"]:
+            path = request.form["url_image"]
         if path:
             filename = "image.png"
             deck = scan(path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return render_template("upload.html", deck=deck, image=filename, form=url_form)
+    return render_template("upload.html", deck=deck, image=filename)
