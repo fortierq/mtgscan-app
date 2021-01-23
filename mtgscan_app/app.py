279af380469c36a55faaa64279a4d8206855add4
Quentin Fortier <qpfortier@gmail.com>
2021-01-23 22:31:29 +0100

    Revert "Move initialization to __init__"
    This reverts commit a7d70a552184987d86c2edb2e0a35d5bddb572e8.

--- mtgscan_app/app.py
+++ mtgscan_app/app.py
@@ -1,9 +1,44 @@
+import os
+import threading
 import time
+from pathlib import Path
 
-from flask import render_template, request, send_from_directory, jsonify
+from flask import Flask, render_template, request, send_from_directory
+from mtgscan.ocr import Azure
+from mtgscan.text import MagicRecognition
 from werkzeug.utils import secure_filename
 
-from mtgscan_app import app, azure, rec
+from mtgscan_app.scan import scan
+
+DIR_ROOT = Path(__file__).parents[1]
+azure = Azure()
+rec = None
+
+UPLOAD_FOLDER = Path(__file__).parent / "dl"
+UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 100_000_000
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.before_first_request
+def before_first_request():
+    def load():
+        """
+        This function cleans up old tasks from our in-memory data structure.
+        """
+        global rec
+
+        rec = MagicRecognition(file_all_cards=str(DIR_ROOT / "all_cards.txt"),
+                               file_keywords=(DIR_ROOT / "Keywords.json"),
+                               max_ratio_diff=0.2)
+
+    thread = threading.Thread(target=load)
+    thread.start()
 
 
 @app.route('/uploads/<filename>')
@@ -29,8 +64,3 @@ def upload_file():
             filename = "image.png"
             deck = scan(path, app.config['UPLOAD_FOLDER'] / filename, azure, rec)
     return render_template("upload.html", deck=deck, image=filename)
-
-@app.route('/api/<path:url>')
-def api(url):
-    deck = scan(url, None, azure, rec)
-    return jsonify(deck.maindeck)
