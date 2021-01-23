a7d70a552184987d86c2edb2e0a35d5bddb572e8
Quentin Fortier <qpfortier@gmail.com>
2021-01-23 22:17:25 +0100

    Move initialization to __init__

--- mtgscan_app/__init__.py
+++ mtgscan_app/__init__.py
@@ -1,1 +1,36 @@
-__version__ = '0.1.0'
+import os
+import threading
+from pathlib import Path
+
+from flask import Flask
+from mtgscan.ocr import Azure
+from mtgscan.text import MagicRecognition
+
+print(1)
+
+DIR_ROOT = Path(__file__).parents[1]
+azure = Azure()
+rec = None
+
+UPLOAD_FOLDER = DIR_ROOT / "dl"
+UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 100_000_000
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+# Load cards
+def load():
+    global rec
+
+    rec = MagicRecognition(file_all_cards=str(DIR_ROOT / "all_cards.txt"),
+                           file_keywords=(DIR_ROOT / "Keywords.json"),
+                           max_ratio_diff=0.2)
+
+
+if not rec:
+    thread = threading.Thread(target=load)
+    thread.start()
--- mtgscan_app/scan.py
+++ mtgscan_app/scan.py
@@ -4,6 +4,7 @@ def scan(image, path, azure, rec):
     box_texts = azure.image_to_box_texts(image)
     box_cards = rec.box_texts_to_cards(box_texts)
     rec.assign_stacked(box_texts, box_cards)
-    box_cards.save_image(image, path)
+    if path:
+        box_cards.save_image(image, path)
     deck = rec.box_texts_to_deck(box_texts)
     return deck
