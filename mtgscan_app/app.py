2add5db2b180dcb76ea0b9fc2b0f7affae18c4bb
Quentin Fortier <qpfortier@gmail.com>
2021-02-03 21:51:53 +0100

    Use celery for loading cards

--- mtgscan_app/app.py
+++ mtgscan_app/app.py
@@ -1,5 +1,4 @@
 import os
-import threading
 import time
 from pathlib import Path
 
@@ -9,7 +8,7 @@ from mtgscan.ocr import Azure
 from mtgscan.text import MagicRecognition
 from werkzeug.utils import secure_filename
 
-from mtgscan_app.scan import scan
+from .scan import scan
 
 DIR_ROOT = Path(__file__).parents[1]
 azure = Azure()
@@ -19,37 +18,42 @@ UPLOAD_FOLDER = Path(__file__).parent / "dl"
 UPLOAD_FOLDER.mkdir(parents=True, exist_ok=True)
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+# Initialize Flask
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 100_000_000
 app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# Celery configuration
+# Initialize Celery
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-
-# Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 
-def load():
-    global rec
-     
-    if rec is None:
-        rec = MagicRecognition(file_all_cards=str(DIR_ROOT / "all_cards.txt"),
-                               file_keywords=(DIR_ROOT / "Keywords.json"),
-                               max_ratio_diff=0.2)
+@celery.task
+def load_cards():
+    return {
+        "cards":
+        MagicRecognition(file_all_cards=str(DIR_ROOT / "all_cards.txt"),
+                         file_keywords=(DIR_ROOT / "Keywords.json"),
+                         max_ratio_diff=0.2)
+    }
 
 
-thread = threading.Thread(target=load)
-thread.start()
+@app.before_first_request
+def init():
+    global task_load_cards
+    task_load_cards = load_cards.delay()
 
 
 def wait_rec():  # wait until rec files are loaded
-    while not rec:
+    global rec
+    result = load_cards.AsyncResult(task_load_cards.id)
+    while not rec and result.state == "PENDING":
         time.sleep(5)
+    rec = task_load_cards.info.get("cards", None)
 
 
 @app.route('/uploads/<filename>')
