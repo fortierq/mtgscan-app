463b1a6b23a812b633d3b0371d82718c982d8695
Quentin Fortier <qpfortier@gmail.com>
2021-02-09 21:52:33 +0100

    Use celery.Task custom class

--- mtgscan_app/app.py
+++ mtgscan_app/app.py
@@ -4,7 +4,7 @@ eventlet.monkey_patch()
 import os
 from pathlib import Path
 
-from celery import Celery
+from celery import Celery, Task
 from flask import Flask, jsonify, render_template
 from mtgscan.ocr import Azure
 from mtgscan.text import MagicRecognition
@@ -22,24 +22,43 @@ socketio = SocketIO(app, message_queue=REDIS_URL)
 celery = Celery(app.name)
 celery.conf.update(CELERY_BROKER_URL=REDIS_URL, CELERY_RESULT_BACKEND=REDIS_URL)
 
+
+class ScanTask(Task):
+    _rec = None
+
+    def __init__(self):
+        self._rec = MagicRecognition(file_all_cards=str(DIR_ROOT / "all_cards.txt"),
+                                     file_keywords=(DIR_ROOT / "Keywords.json"),
+                                     max_ratio_diff=0.2)
+
+    def scan(self, image):
+        azure = Azure()
+        box_texts = azure.image_to_box_texts(image)
+        box_cards = self._rec.box_texts_to_cards(box_texts)
+        self._rec.assign_stacked(box_texts, box_cards)
+        box_cards.save_image(image, "image.png")
+        deck = self._rec.box_texts_to_deck(box_texts)
+        return deck
+
+
 @app.route("/")
 def index():
     return render_template("upload.html")
 
+
 @socketio.on("scan")
 def scan_io(msg):
-    scan_celery.apply_async((msg["image"],))
+    scan_celery.apply_async((msg["image"], ))
 
-@celery.task
+
+@celery.task(base=ScanTask)
 def scan_celery(image):
-    deck = scan(image)
+    deck = scan_celery.scan(image)
     sio = SocketIO(message_queue=REDIS_URL)
     sio.emit("scan_result", {"deck": deck.maindeck.cards, "image": ""})
 
-def scan(image):
-    rec = MagicRecognition(file_all_cards=str(DIR_ROOT / "all_cards.txt"),
-                           file_keywords=(DIR_ROOT / "Keywords.json"),
-                           max_ratio_diff=0.2)
+
+def scan(rec, image):
     azure = Azure()
     box_texts = azure.image_to_box_texts(image)
     box_cards = rec.box_texts_to_cards(box_texts)
@@ -48,10 +67,12 @@ def scan(image):
     deck = rec.box_texts_to_deck(box_texts)
     return deck
 
+
 @app.route("/api/<path:url>")
 def api_scan(url):
     deck = scan(url)
     return jsonify({"maindeck": deck.maindeck.cards, "sideboard": deck.sideboard.cards})
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
