a2d191bcab443ef3bca35e493e608296ed874f7a
Quentin Fortier <qpfortier@gmail.com>
2021-02-10 22:14:58 +0100

    Start and shutdown Redis

--- run_flask.sh
+++ run_flask.sh
@@ -1,9 +1,9 @@
-trap "kill 0" EXIT # to kill all subprocesses when done
-
 set -a
 source prod.env # set environment variables
 set +a
 
-$REDIS & # run Redis as message broker
+redis-server # run Redis as message broker
 
 poetry run python mtgscan_app/app.py # run Flask
+
+redis-cli shutdown
